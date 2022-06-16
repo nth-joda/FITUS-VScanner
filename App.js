11585b780695f9e16fffcfd1ead7dc28c30cff5f
Nth-joda <nthau.2k@gmail.com>
2022-06-16 13:33:03 +0700

    take photo

--- App.js
+++ App.js
@@ -1,24 +1,11 @@
-import {
-  StyleSheet,
-  SafeAreaView,
-  View,
-  Platform,
-  Dimensions,
-  Text,
-  StatusBar,
-} from "react-native";
+import { StyleSheet, Platform, StatusBar } from "react-native";
+import MainScreen from "./app/screens/MainScreen";
+import TakePhotoScreen from "./app/screens/TakePhotoScreen";
 
 export default function App() {
   console.log("APP EXECUTED");
-  const img_uri =
-    "https://images.unsplash.com/photo-1534270804882-6b5048b1c1fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80";
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.layout}>
-        <Text>dasd</Text>
-      </View>
-    </SafeAreaView>
-  );
+
+  return <TakePhotoScreen />;
 }
 
 const styles = StyleSheet.create({
